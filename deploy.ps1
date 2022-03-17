be50d6fda33955ff6af6e90254238fa0db94e30c
Travis Watson <watsontravis@protonmail.com>
2022-03-17 16:07:01 -0500

    syntax

--- deploy.ps1
+++ deploy.ps1
@@ -4,7 +4,7 @@ New-AzResourceGroup -Name "Test" -Location "West US" -Force
 $config = @{
     appServiceName = 'ConfigName'
     location       = 'West US'
-    webAppNames       = @{'App1', 'App2'}
+    webAppNames    = @('App1', 'App2')
 }
 
 New-AzResourceGroupDeployment -ResourceGroupName "Test" -TemplateFile ".\main.bicep" -TemplateParameterFile ".\params\dev.parameters.json" @config
